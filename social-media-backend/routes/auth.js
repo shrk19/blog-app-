9e639fd25181c3874c5785f4f7dada2c3d711002
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-12-20 23:07:26 +0530

    changes

--- social-media-backend/routes/auth.js
+++ social-media-backend/routes/auth.js
@@ -1,12 +1,13 @@
-import express from 'express'
-import { signup, signin, logout, refetch } from '../controllers/auth.js'
-import { verifyToken } from '../verifyToken.js';
+import express from "express";
+import { signup, signin, logout, refetch } from "../controllers/auth.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-router.post('/signup', signup)
-router.post('/signin', signin)
-router.get('/logout', logout)
-router.get('/refetch', verifyToken, refetch)
+router
+  .post("/signup", signup)
+  .post("/signin", signin)
+  .get("/logout", logout)
+  .get("/refetch", verifyToken, refetch);
 
 export default router;
